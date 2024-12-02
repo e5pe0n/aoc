ede840ed361328253816dd598b29e12d6d7994ec
e5pe0n <1izard.synchr0fi5h@gmail.com>
2024-12-02 21:53:37 +0900

    day2 part2 ts

---
+++ 2024/ts/02/part02.ts
@@ -0,0 +1,70 @@
+import fs from "node:fs";
+import * as path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export function solve(filename: string) {
+  const fp = path.resolve(path.join(__dirname, filename));
+  const s = fs.readFileSync(fp, "utf-8");
+  const reports: number[][] = [];
+  for (const line of s.split("\n").slice(0, -1)) {
+    reports.push(line.split(/\s+/).map(Number));
+  }
+  let sum = 0;
+  for (let i = 0; i < reports.length; ++i) {
+    const diffs: number[] = [];
+    for (let j = 0; j < reports[i]!.length - 1; ++j) {
+      diffs.push(reports[i]![j]! - reports[i]![j + 1]!);
+    }
+    let safe = true;
+    for (let j = 0; j < diffs.length - 1; ++j) {
+      if (diffs[j]! * diffs[j + 1]! < 0) {
+        safe = false;
+        break;
+      }
+    }
+    const abss = diffs.map(Math.abs);
+    if (!(1 <= Math.min(...abss) && Math.max(...abss) <= 3)) {
+      safe = false;
+    }
+    if (safe) {
+      ++sum;
+      continue;
+    }
+
+    for (let k = 0; k < reports[i]!.length; ++k) {
+      const newReport: number[] = [];
+      for (let j = 0; j < reports[i]!.length; ++j) {
+        if (j === k) {
+          continue;
+        }
+        newReport.push(reports[i]![j]!);
+      }
+      const diffs: number[] = [];
+      for (let j = 0; j < newReport.length - 1; ++j) {
+        diffs.push(newReport[j]! - newReport[j + 1]!);
+      }
+      let safe = true;
+      for (let j = 0; j < diffs.length - 1; ++j) {
+        if (diffs[j]! * diffs[j + 1]! < 0) {
+          safe = false;
+          break;
+        }
+      }
+      if (!safe) {
+        continue;
+      }
+      const abss = diffs.map(Math.abs);
+      if (!(1 <= Math.min(...abss) && Math.max(...abss) <= 3)) {
+        safe = false;
+      }
+      if (safe) {
+        ++sum;
+        break;
+      }
+    }
+  }
+  return sum;
+}
