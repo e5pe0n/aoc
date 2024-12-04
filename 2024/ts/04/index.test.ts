93dcab6fa239707e28e4bbeab7282da217f3e90c
e5pe0n <1izard.synchr0fi5h@gmail.com>
2024-12-04 21:23:08 +0900

    day4 part2 ts

--- 2024/ts/04/index.test.ts
+++ 2024/ts/04/index.test.ts
@@ -2,7 +2,7 @@ import { test, expect, describe } from "vitest";
 import { solve as solve01 } from "./part01.js";
 import { solve as solve02 } from "./part02.js";
 
-describe.only("part01", () => {
+describe("part01", () => {
   test("example", () => {
     const res = solve01("input-example.txt");
     expect(res).toBe(18);
@@ -15,11 +15,11 @@ describe.only("part01", () => {
 
 describe("part02", () => {
   test("example", () => {
-    const res = solve02("input-example-02.txt");
-    expect(res).toBe(48);
+    const res = solve02("input-example.txt");
+    expect(res).toBe(9);
   });
   test("input", () => {
     const res = solve02("input.txt");
-    expect(res).toBe(104245808);
+    expect(res).toBe(1900);
   });
 });
--- 2024/ts/04/part02.ts
+++ 2024/ts/04/part02.ts
@@ -7,52 +7,16 @@ const __dirname = path.dirname(__filename);
 
 export function solve(filename: string): number {
   const fp = path.resolve(path.join(__dirname, filename));
-  const s = fs.readFileSync(fp, "utf-8");
-  const MUL_RE = /mul\((\d{1,3}),(\d{1,3})\)/g;
-  const DO_RE = /do\(\)/g;
-  const DONT_RE = /don't\(\)/g;
-  const muls = s.matchAll(MUL_RE);
-  const dos = s.matchAll(DO_RE);
-  const donts = s.matchAll(DONT_RE);
+  const s = fs.readFileSync(fp, "utf-8").split("\n").slice(0, -1);
 
-  const ms: RegExpExecArray[] = [];
-  for (const m of muls) {
-    ms.push(m);
-  }
-  const doIdxs: number[] = [];
-  for (const d of dos) {
-    doIdxs.push(d.index);
-  }
-  const dontIdxs: number[] = [];
-  for (const d of donts) {
-    dontIdxs.push(d.index);
-  }
-
-  let mi = 0;
-  let doi = 0;
-  let donti = 0;
-  let enabled = true;
   let sum = 0;
-  for (let i = 0; i < s.length; ++i) {
-    const doIdx = doIdxs[doi];
-    const dontIdx = dontIdxs[donti];
-    const m = ms[mi];
-    if (i === doIdx) {
-      enabled = true;
-      ++doi;
-      continue;
-    }
-    if (i === dontIdx) {
-      enabled = false;
-      ++donti;
-      continue;
-    }
-    if (!!m && i === m.index) {
-      if (enabled) {
-        const [, a, b] = m;
-        sum += Number.parseInt(a!) * Number.parseInt(b!);
+  for (let i = 1; i < s.length - 1; ++i) {
+    for (let j = 1; j < s[0]!.length - 1; ++j) {
+      const t1 = s[i - 1]![j - 1]! + s[i]![j]! + s[i + 1]![j + 1]!;
+      const t2 = s[i - 1]![j + 1]! + s[i]![j]! + s[i + 1]![j - 1]!;
+      if ((t1 === "MAS" || t1 === "SAM") && (t2 === "MAS" || t2 === "SAM")) {
+        ++sum;
       }
-      ++mi;
     }
   }
   return sum;
