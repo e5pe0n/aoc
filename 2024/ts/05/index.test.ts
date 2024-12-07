222808361d534326242a65e4db33096ad46e7bf0
e5pe0n <1izard.synchr0fi5h@gmail.com>
2024-12-07 12:28:43 +0900

    day5 part2 ts

--- 2024/ts/05/index.test.ts
+++ 2024/ts/05/index.test.ts
@@ -2,7 +2,7 @@ import { test, expect, describe } from "vitest";
 import { solve as solve01 } from "./part01.js";
 import { solve as solve02 } from "./part02.js";
 
-describe.only("part01", () => {
+describe("part01", () => {
   test("example", () => {
     const res = solve01("input-example.txt");
     expect(res).toBe(143);
@@ -16,10 +16,10 @@ describe.only("part01", () => {
 describe("part02", () => {
   test("example", () => {
     const res = solve02("input-example.txt");
-    expect(res).toBe();
+    expect(res).toBe(123);
   });
   test("input", () => {
     const res = solve02("input.txt");
-    expect(res).toBe();
+    expect(res).toBe(4828);
   });
 });
--- 2024/ts/05/part02.ts
+++ 2024/ts/05/part02.ts
@@ -1,14 +1,68 @@
 import fs from "node:fs";
 import * as path from "node:path";
 import { fileURLToPath } from "node:url";
+import { range } from "../lib.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+type Rule = {
+  left: number;
+  right: number;
+};
+
 export function solve(filename: string): number {
   const fp = path.resolve(path.join(__dirname, filename));
-  const s = fs.readFileSync(fp, "utf-8").split("\n").slice(0, -1);
+  const [ruleLines, updateLines] = fs
+    .readFileSync(fp, "utf-8")
+    .split("\n\n") as [string, string];
+  const rules: Rule[] = ruleLines
+    .split("\n")
+    .map((line) =>
+      line
+        .trim()
+        .split("|")
+        .map((v) => Number(v)),
+    )
+    .map(([left, right]) => ({ left: left!, right: right! }));
+  const updates: number[][] = updateLines
+    .split("\n")
+    .map((line) => line.trim())
+    .filter((line) => line.length > 0)
+    .map((line) => line.split(",").map(Number));
 
-  let sum = 0;
-  return sum;
+  const ruleMap = new Map<number, Set<number>>();
+  for (const rule of rules) {
+    ruleMap.set(
+      rule.left,
+      (ruleMap.get(rule.left) ?? new Set()).add(rule.right),
+    );
+  }
+
+  let res = 0;
+  // for (const update of updates.slice(0, 1)) {
+  for (const update of updates) {
+    const sortedUpdate = [...update].sort((a, b) => {
+      if (ruleMap.get(a)?.has(b)) {
+        return -1;
+      }
+      if (ruleMap.get(b)?.has(a)) {
+        return 1;
+      }
+      return 0;
+    });
+    // console.log("update:", update);
+    // console.log("sortedUpdate:", sortedUpdate);
+    let alreadySorted = true;
+    for (let i = 0; i < update.length; ++i) {
+      if (sortedUpdate[i] !== update[i]) {
+        alreadySorted = false;
+        break;
+      }
+    }
+    if (!alreadySorted) {
+      res += sortedUpdate[Math.floor(update.length / 2)]!;
+    }
+  }
+  return res;
 }
