867535b7f084b108664a6ea0943231c8f370f77c
e5pe0n <1izard.synchr0fi5h@gmail.com>
2024-12-10 22:04:09 +0900

    day10 part2 ts

--- 2024/ts/10/index.test.ts
+++ 2024/ts/10/index.test.ts
@@ -2,7 +2,7 @@ import { test, expect, describe } from "vitest";
 import { solve as solve01 } from "./part01.js";
 import { solve as solve02 } from "./part02.js";
 
-describe.only("part01", () => {
+describe("part01", () => {
   test("example", () => {
     const res = solve01("input-example.txt");
     expect(res).toBe(36);
@@ -16,10 +16,10 @@ describe.only("part01", () => {
 describe("part02", () => {
   test("example", () => {
     const res = solve02("input-example.txt");
-    expect(res).toBe();
+    expect(res).toBe(81);
   });
   test("input", () => {
     const res = solve02("input.txt");
-    expect(res).toBe(); // 312ms
+    expect(res).toBe(1186);
   });
 });
--- 2024/ts/10/part02.ts
+++ 2024/ts/10/part02.ts
@@ -6,74 +6,61 @@ import { range } from "../lib.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-type File = {
-  id: number;
-  ptr: number;
-  length: number;
-  prev?: File;
-  next?: File;
-};
+const didj = [
+  [-1, 0],
+  [1, 0],
+  [0, -1],
+  [0, 1],
+] as const;
 
 export function solve(filename: string): number {
   const fp = path.resolve(path.join(__dirname, filename));
   const content = fs.readFileSync(fp, "utf-8").trim();
-  const memory = content.split("").map(Number);
-  let ptr = 0;
-  const root: File = { id: 0, ptr, length: memory[0]! };
-  ptr += root.length;
-  let curr: File | undefined = root;
-  for (let i = 1; i < memory.length; ++i) {
-    if (i % 2 === 0) {
-      const file: File = {
-        id: curr!.id + 1,
-        ptr,
-        length: memory[i]!,
-        prev: curr,
-      };
-      ptr += file.length;
-      curr.next = file;
-      curr = file;
-    } else {
-      ptr += memory[i]!;
-    }
+  const M = content.split("\n").map((line) => line.split("").map(Number));
+  const H = M.length;
+  const W = M[0]!.length;
+
+  function inMap(i: number, j: number): boolean {
+    return 0 <= i && i < H && 0 <= j && j < W;
   }
-  const touched = range(Math.ceil(memory.length / 2)).map(() => false);
-  while (curr) {
-    let head = root;
-    const nextCurr: File | undefined = curr.prev;
-    if (!touched[curr.id]) {
-      while (head.next && head.ptr < curr.ptr) {
-        const space = head.next.ptr - (head.ptr + head.length);
-        if (space >= curr.length) {
-          curr.ptr = head.ptr + head.length;
-          if (curr.prev) {
-            curr.prev.next = curr.next;
-          }
-          if (curr.next) {
-            curr.next.prev = curr.prev;
-          }
-          const tail = head.next;
-          head.next = curr;
-          curr.prev = head;
-          curr.next = tail;
-          tail.prev = curr;
-          break;
+
+  function bfs(i: number, j: number): number {
+    // const visited = range(H).map(() => range(W).map(() => false));
+    const q: [number, number, number][] = [[i, j, 0]];
+    let score = 0;
+    while (q.length > 0) {
+      const [i, j, h] = q.pop()!;
+      // if (visited[i]![j]) {
+      //   continue;
+      // }
+      // visited[i]![j] = true;
+      if (M[i]![j] === 9) {
+        ++score;
+        continue;
+      }
+      for (const [di, dj] of didj) {
+        const ni = i + di;
+        const nj = j + dj;
+        if (
+          inMap(ni, nj) &&
+          // !visited[ni]![nj] &&
+          M[ni]![nj]! - M[i]![j]! === 1
+        ) {
+          q.push([ni, nj, h + 1]);
         }
-        head = head.next;
       }
     }
-    touched[curr.id] = true;
-    curr = nextCurr;
+    return score;
   }
-  curr = root;
+
   let res = 0;
-  while (curr) {
-    const sum =
-      curr.id *
-      (((curr.ptr + curr.length - 1) * (curr.ptr + curr.length)) / 2 -
-        ((curr.ptr - 1) * curr.ptr) / 2);
-    res += sum;
-    curr = curr.next;
+  for (let i = 0; i < H; ++i) {
+    for (let j = 0; j < W; ++j) {
+      if (M[i]![j] === 0) {
+        const score = bfs(i, j);
+        res += score;
+      }
+    }
   }
   return res;
 }
