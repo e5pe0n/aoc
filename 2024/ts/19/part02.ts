79e84afd4c236e2a3445abc52df61b0cbdecd7f5
e5pe0n <1izard.synchr0fi5h@gmail.com>
2024-12-28 14:08:56 +0900

    day19 part2 ts

---
+++ 2024/ts/19/part02.ts
@@ -0,0 +1,40 @@
+import fs from "node:fs";
+import * as path from "node:path";
+import { fileURLToPath } from "node:url";
+import { range } from "../lib.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export function solve(filename: string): number {
+  const fp = path.resolve(path.join(__dirname, filename));
+  const lines = fs.readFileSync(fp, "utf-8").trim().split("\n");
+  const towels = lines[0]!.split(/,\s*/);
+  const designs = lines.slice(2);
+
+  function f(design: string): number {
+    const dp = range(design.length + 1).map(() => 0);
+    dp[0] = 1;
+    for (let i = 0; i <= design.length; ++i) {
+      if (dp[i] === 0) {
+        continue;
+      }
+      for (const towel of towels) {
+        if (
+          i + towel.length <= design.length &&
+          design.substring(i, i + towel.length) === towel
+        ) {
+          dp[i + towel.length]! += dp[i]!;
+        }
+      }
+    }
+    return dp[design.length]!;
+  }
+
+  let res = 0;
+  for (const design of designs) {
+    res += f(design);
+  }
+
+  return res;
+}
