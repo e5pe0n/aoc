750322f04d8402c609b0e915a8e1556010ef32c5
e5pe0n <1izard.synchr0fi5h@gmail.com>
2025-12-04 18:25:28 +0900

    2025 day01 part2

--- 2025/rs/examples/day01/main.rs
+++ 2025/rs/examples/day01/main.rs
@@ -1,5 +1,31 @@
 use std::fs;
 
+// fn main() {
+//     let content = fs::read_to_string("./input.txt").unwrap();
+//     let lines = content
+//         .split('\n')
+//         .filter(|line| !line.is_empty())
+//         .collect::<Vec<_>>();
+//     let mut sum = 50;
+//     let mut password = 0;
+//     for line in lines {
+//         let chars = line.chars().collect::<Vec<_>>();
+//         let lr = &chars[0];
+//         let n = (&chars[1..].iter().collect::<String>())
+//             .parse::<i64>()
+//             .unwrap();
+//         if lr == &'L' {
+//             sum = (sum - n + 100) % 100;
+//         } else {
+//             sum = (sum + n) % 100;
+//         }
+//         if sum == 0 {
+//             password += 1;
+//         }
+//     }
+//     println!("{}", password);    // 964
+// }
+
 fn main() {
     let content = fs::read_to_string("./input.txt").unwrap();
     let lines = content
@@ -15,13 +41,12 @@ fn main() {
             .parse::<i64>()
             .unwrap();
         if lr == &'L' {
-            sum = (sum - n + 100) % 100;
+            password += -(((sum - 100) % 100 - n) / 100);
+            sum = (100 + ((sum - n) % 100)) % 100;
         } else {
+            password += (sum + n) / 100;
             sum = (sum + n) % 100;
         }
-        if sum == 0 {
-            password += 1;
-        }
     }
-    println!("{}", password);
+    println!("{}", password); // 5872
 }
