4062c26c8a5870d10b10c7ce5ffc2e2bf766052c
e5pe0n <1izard.synchr0fi5h@gmail.com>
2025-12-07 21:48:18 +0900

    day05 part2

--- 2025/rs/examples/day05/main.rs
+++ 2025/rs/examples/day05/main.rs
@@ -1,5 +1,44 @@
 use std::{collections::HashSet, fs};
 
+// fn main() {
+//     // let content = fs::read_to_string("./input_example.txt").unwrap();
+//     let content = fs::read_to_string("./input.txt").unwrap();
+//     let lines = content
+//         .split('\n')
+//         .filter_map(|line| {
+//             let trimmed = line.trim();
+//             if trimmed.is_empty() {
+//                 None
+//             } else {
+//                 Some(trimmed)
+//             }
+//         })
+//         .collect::<Vec<_>>();
+
+//     let rs = lines
+//         .iter()
+//         .take_while(|line| line.contains('-'))
+//         .map(|line| {
+//             let r = line.split('-').collect::<Vec<_>>();
+//             return (r[0].parse::<i64>().unwrap(), r[1].parse::<i64>().unwrap());
+//         })
+//         .collect::<Vec<_>>();
+//     let ids = lines
+//         .iter()
+//         .skip_while(|line| line.contains('-'))
+//         .map(|line| line.parse::<i64>().unwrap())
+//         .collect::<Vec<_>>();
+//     let mut res: HashSet<i64> = HashSet::new();
+//     for (l, r) in rs {
+//         for id in &ids {
+//             if (l..=r).contains(&id) {
+//                 res.insert(*id);
+//             }
+//         }
+//     }
+//     println!("{}", res.len()); // 563
+// }
+
 fn main() {
     // let content = fs::read_to_string("./input_example.txt").unwrap();
     let content = fs::read_to_string("./input.txt").unwrap();
@@ -15,7 +54,7 @@ fn main() {
         })
         .collect::<Vec<_>>();
 
-    let rs = lines
+    let mut rs = lines
         .iter()
         .take_while(|line| line.contains('-'))
         .map(|line| {
@@ -23,18 +62,19 @@ fn main() {
             return (r[0].parse::<i64>().unwrap(), r[1].parse::<i64>().unwrap());
         })
         .collect::<Vec<_>>();
-    let ids = lines
-        .iter()
-        .skip_while(|line| line.contains('-'))
-        .map(|line| line.parse::<i64>().unwrap())
-        .collect::<Vec<_>>();
-    let mut res: HashSet<i64> = HashSet::new();
-    for (l, r) in rs {
-        for id in &ids {
-            if (l..=r).contains(&id) {
-                res.insert(*id);
-            }
+    rs.sort_by(|a, b| a.0.cmp(&b.0));
+    let (mut l, mut r) = rs[0];
+    let mut res = r - l + 1;
+    for (ll, rr) in rs.iter().skip(1) {
+        if rr <= &r {
+            continue;
+        } else if &r < ll {
+            res += rr - ll + 1;
+            (l, r) = (*ll, *rr);
+        } else if ll <= &r {
+            res += rr - r;
+            (l, r) = (*ll, *rr);
         }
     }
-    println!("{}", res.len()); // 563
+    println!("{res}");
 }
