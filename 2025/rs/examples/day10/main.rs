17d588a1562ab4ae7ea3461b63756f112b360263
e5pe0n <1izard.synchr0fi5h@gmail.com>
2025-12-10 19:13:27 +0900

    day10 part1

---
+++ 2025/rs/examples/day10/main.rs
@@ -0,0 +1,68 @@
+use std::{
+    collections::{HashMap, VecDeque},
+    fs,
+};
+
+fn main() {
+    // let content = fs::read_to_string("input_example.txt").unwrap();
+    let content = fs::read_to_string("input.txt").unwrap();
+    let lines = content
+        .split('\n')
+        .filter_map(|line| {
+            let trimmed = line.trim();
+            if trimmed.is_empty() {
+                None
+            } else {
+                Some(trimmed)
+            }
+        })
+        .collect::<Vec<_>>();
+    let mut sum = 0i64;
+    for line in lines {
+        let ss = line.split(' ').collect::<Vec<_>>();
+        let light_str = &ss[0][1..ss[0].len() - 1];
+        let mut light = 0u16;
+        for (i, char) in light_str.chars().rev().enumerate() {
+            if char == '#' {
+                light |= 1 << i;
+            }
+        }
+        let num_lights = light_str.len() as u16;
+        let buttons = &ss[1..ss.len() - 1]
+            .iter()
+            .map(|s| {
+                let ns = &s[1..s.len() - 1]
+                    .split(',')
+                    .map(|t| return t.parse::<u16>().unwrap())
+                    .fold(0u16, |acc, v| {
+                        let acc = acc | 1 << (num_lights - 1 - v);
+                        acc
+                    });
+                return ns.clone();
+            })
+            .collect::<Vec<_>>();
+
+        let mut g: HashMap<u16, Vec<u16>> = (0..(1 << num_lights)).map(|n| (n, vec![])).collect();
+
+        for u in 0..(1 << num_lights) {
+            for button in buttons {
+                g.entry(u).and_modify(|v| v.push(u ^ button));
+            }
+        }
+
+        let mut q: VecDeque<(u16, u16)> = VecDeque::new();
+        q.push_back((0, 0));
+        let mut dist = 0;
+        while let Some((u, d)) = q.pop_front() {
+            if u == light {
+                dist = d;
+                break;
+            }
+            for v in g.get(&u).unwrap() {
+                q.push_back((*v, d + 1));
+            }
+        }
+        sum += dist as i64;
+    }
+    println!("{sum}"); // 512
+}
